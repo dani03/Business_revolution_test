37cadca88615ab489464265d8127e0229c908795
dani03 <lesamessipa@gmail.com>
2020-09-22 18:04:13 +0200

    deleted alert

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var nb_input = form.getElementsByTagName('input').length;
 var progress_bar = document.getElementById('progress_bar');
 var formulaire = document.getElementById('formulaire');
 var reset = document.getElementById('reset');
-
+var pourcentage_fait = 0;
 var actualWidth = document.getElementById('progress_status').offsetWidth;
 var pourcentage_de_base = 0;
+var widthToAdd = 0;
+var pourcentage = 0;
 
 //function permettant de recupérer l'id precis d'une tâche
 // et traitement des checkbox
@@ -16,18 +18,29 @@ function getId(element){
  
  if(l_id.checked){
     //on divise la width par le nombre d'input  soit 100% de la width
-    var pourcentage = Math.trunc(100 / nb_input);
+    pourcentage = Math.trunc(100 / nb_input);
     pourcentage_de_base += pourcentage; 
-    var widthToAdd = Math.trunc(actualWidth / nb_input);  
+    widthToAdd = Math.trunc(actualWidth / nb_input);  
     progress_bar.style.width = progress_bar.offsetWidth + widthToAdd+ "px";
-    var pourcentage_fait = pourcentage_de_base >= 98 ? 100 : pourcentage_de_base;
+    pourcentage_fait = pourcentage_de_base >= 98 ? 100 : pourcentage_de_base;
     progress_bar.innerHTML = pourcentage_fait +" %";
     if(pourcentage_fait == 100){
       document.getElementById('message').style.display = "block";
       document.getElementById('message').innerHTML = "<h2>Félicitations toutes les tâches sont faites.</h2>";
     }  
- } else {
-    alert("pas cochez");
+ } else if(!l_id.checked) {
+ 
+    pourcentage = Math.trunc(100 / nb_input);
+    pourcentage_de_base = (pourcentage_de_base == 0) ? 0 : pourcentage_de_base - pourcentage; 
+    widthToAdd = Math.trunc(actualWidth / nb_input);  
+    progress_bar.style.width = progress_bar.offsetWidth - widthToAdd+ "px";
+    pourcentage_fait = pourcentage_de_base >= 98 ? 100 : pourcentage_de_base;
+    progress_bar.innerHTML = pourcentage_fait +" %";
+    if(pourcentage_fait !== 100){
+      document.getElementById('message').style.display = "none";
+      document.getElementById('message').innerHTML = "<h2>Félicitations toutes les tâches sont faites.</h2>";
+    }  
+   
  }
 
 }
